046a554cac8903585e30ef138a12297f397af8bc
Yuvraj Soni <demonsroar2172@gmail.com>
2023-12-15 20:24:34 +0530

    authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <Button size="lg" variant="destructive">
-      Click me{" "}
-    </Button>
+    <div className="flex flex-col gap-y-4">
+      <h1>Dashboard</h1>
+      <UserButton afterSignOutUrl="/" />
+    </div>
   );
 }
